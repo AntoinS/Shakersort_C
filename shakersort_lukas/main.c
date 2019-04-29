def4a398ddce0a2d16f890a1de976a2dbd19a716
Lukas Fleper <40719030+lfleper@users.noreply.github.com>
2019-04-29 15:43:19 +0200

    Add files via upload

---
+++ shakersort_lukas/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+int main()
+{
+    int *paiFeld;   // Pointer Array
+    int iAnz;       // Anzahl der Werte die Eingegeben werden
+    int iX;         // Laufvariable
+
+    printf("Anzahl der Werte: ");
+    scanf("%d", &iAnz);
+    //fflush(stdin);          // <-- nicht unbedingt Notwendig, da es eh nur unter Windows funktioniert aber Frau Hohmann steht drauf
+
+    // Speicherplatz für die Anzahl einzugebener Werte reservieren
+    paiFeld = (int *)malloc(iAnz * sizeof(int));
+
+    // Werte einlesen
+    for(iX = 0; iX < iAnz; iX++) {
+        printf("Wert %d: ", iX+1);
+        scanf("%d", paiFeld + iX);
+    }
+
+    // Array und Anzahl zum sortieren übergeben
+    shakersort(paiFeld, iAnz);
+
+    // Ausgabe der Werte
+    for(iX = 0; iX < iAnz; iX++) {
+        printf("%d ", *(paiFeld + iX));
+    }
+
+    // Speicherplatz freigeben
+    free(paiFeld);
+
+    return EXIT_SUCCESS;
+}
---
+++ shakersort_lukas/sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+#include "swap.h"
+
+void shakersort(int *paiFeld, int iAnz) {
+    int iX;
+    int iY;
+
+    for(iX = 1; iX <= iAnz / 2; iX++) {
+        for(iY = iX - 1; iY < iAnz - iX; iY++) {
+            if(*(paiFeld + iY) > *(paiFeld + iY + 1)) {
+                swap(paiFeld + iY, paiFeld + iY + 1);
+            }
+        }
+
+        for(iY = iAnz - iX - 1; iY >= iX; iY--) {
+            if(*(paiFeld + iY) < *(paiFeld + iY -1)) {
+                swap(paiFeld + iY, paiFeld + iY - 1);
+            }
+        }
+    }
+}
